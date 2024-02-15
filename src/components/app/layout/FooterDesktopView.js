96ca56f331682243e167431246179ab3a1aa6c41
Michael Naguib <drmichaelnaguib.mn@gmail.com>
2024-02-15 11:55:06 +0200

    configured header and footer mobile and desktop views

---
+++ src/components/app/layout/FooterDesktopView.js
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.scss";
+
+const FooterDesktopView = () => {
+  return (
+    <p className={styles["desktop-footer-phrase"]}>
+      Copyright © 2020 Reliance Compliance Solutions - All Rights Reserved.{" "}
+    </p>
+  );
+};
+export default FooterDesktopView;
